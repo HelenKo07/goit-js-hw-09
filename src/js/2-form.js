57b9e5db855b07f9261b28771626d5b1f8723fa4
Alena Korma <alena.gorova93@gmail.com>
2024-12-26 15:29:39 +0200

    commit message

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,36 @@
-console.log('Form');
+const feedbackFormEl = document.querySelector('.feedback-form');
+let formData = {
+    email: '',
+    message: '',
+};
+
+const fillformFields = () => {
+    try {
+        const formDataFromLS = JSON.parse(localStorage.getItem('feedback-form-data'));
+        console.log(formDataFromLS);
+    } catch(err) {
+        console.log(err);
+    }
+};
+
+fillformFields();
+
+const onFormFieldChange = event => {
+const {target: formFieldEl} = event; 
+
+const fieldValue = formFieldEl.value;
+const fieldName = formFieldEl.name;
+
+formData[fieldName] = fieldValue;
+
+localStorage.setItem('feedback-form-data', JSON.stringify(formData));
+// console.log(formData);
+// console.log(fieldValue);
+
+}
+
+feedbackFormEl.addEventListener('change', onFormFieldChange);
+
+
+
+// console.log(feedbackFormEl);
